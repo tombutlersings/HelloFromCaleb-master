17246ec6d0fe93a5011ac252001ba02c518e07cd
tombutlersings <but20021@byui.edu>
2021-05-17 16:57:03 -0700

    Added some text

--- src/HelloFromCaleb.java
+++ src/HelloFromCaleb.java
@@ -6,7 +6,7 @@ public class HelloFromCaleb {
 
     public void greeting(){
         System.out.println("Hello my name is Caleb.");
-        System.out.println("Hello from Tom Butler as well. Good to be here with you! ");
+        System.out.println("Hello from Tom Butler as well. Good to be here with you!  ");
     }
 
     public static void main(String[] args){
