92f890790dbc7c50798bfdc4a024fc1ff04ecce2
tombutlersings <but20021@byui.edu>
2021-05-17 16:46:07 -0700

    Initial commit

---
+++ src/HelloFromCaleb.java
@@ -0,0 +1,16 @@
+
+public class HelloFromCaleb {
+    public HelloFromCaleb(){
+
+    }
+
+    public void greeting(){
+        System.out.println("Hello my name is Caleb.");
+        System.out.println("Hello from Tom Butler as well. Good to be here with you! ");
+    }
+
+    public static void main(String[] args){
+        HelloFromCaleb h = new HelloFromCaleb();
+        h.greeting();
+    }
+}
